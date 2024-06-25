8e6322d12eee3b597e5a035f1e3842b863abe4e6
Khant Kyaw <khantkankei@gmail.com>
2024-06-25 11:31:52 +0630

    code flowchanges and make dynamic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const initialItems = [
 	{ id: 1, description: "Passports", quantity: 2, packed: false },
@@ -9,35 +9,54 @@ const initialItems = [
 function Logo() {
 	return <h1>Far Away</h1>;
 }
-function Form() {
+
+function Form({ addItem }) {
+	const [description, setDescription] = useState("");
+	const [quantity, setQuantity] = useState(1);
+
 	function handleSubmit(e) {
 		e.preventDefault();
-		console.log(e.target.value);
+		const newItem = {
+			id: Date.now(),
+			description,
+			quantity,
+			packed: false,
+		};
+
+		addItem(newItem);
+		setDescription("");
+		setQuantity(1);
 	}
 
 	return (
 		<form className='add-form' onSubmit={handleSubmit}>
 			<h3>What do you need for your trip 👩?</h3>
-			<select>
-				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
-					return (
-						<option value={num} key={num}>
-							{num}
-						</option>
-					);
-				})}
+			<select
+				value={quantity}
+				onChange={(e) => setQuantity(Number(e.target.value))}
+			>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
 			</select>
-			<input type='text' placeholder='Item...' />
+			<input
+				type='text'
+				placeholder='Item...'
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
 			<button>Add</button>
 		</form>
 	);
 }
 
-function PackingList(params) {
+function PackingList({ items }) {
 	return (
 		<div className='list'>
 			<ul>
-				{initialItems.map((item) => (
+				{items.map((item) => (
 					<Item key={item.id} item={item} />
 				))}
 			</ul>
@@ -56,23 +75,32 @@ function Item({ item }) {
 	);
 }
 
-function Stat(params) {
+function Stat({ items }) {
+	const packedItems = items.filter((item) => item.packed).length;
+
 	return (
 		<footer className='stats'>
 			<em>
-				You have X items on your list, and you already packed x items
+				You have {items.length} items on your list, and you already
+				packed {packedItems} items.
 			</em>
 		</footer>
 	);
 }
 
 const App = () => {
+	const [items, setItems] = useState(initialItems);
+
+	const addItem = (item) => {
+		setItems([...items, item]);
+	};
+
 	return (
 		<div className='app'>
 			<Logo />
-			<Form />
-			<PackingList />
-			<Stat />
+			<Form addItem={addItem} />
+			<PackingList items={items} />
+			<Stat items={items} />
 		</div>
 	);
 };
